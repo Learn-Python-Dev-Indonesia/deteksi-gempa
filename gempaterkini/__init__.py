aead733cd199eebe031b096b76e527ae3f8396c4
viko-aldi <piccoaldi@gmail.com>
2022-10-20 22:46:22 +0700

    Second Commit

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -18,19 +18,48 @@ def ekstrasi_data():
         return None
     if content.status_code == 200:
         soup = BeautifulSoup(content.text, 'html.parser')
-        tanggal = soup.find('span', {'class':'waktu'})
-        waktu = tanggal.text.split(', ')[1]
-        tanggal = tanggal.text.split(', ')[0]
+        result = soup.find('span', {'class':'waktu'})
+        result = result.text.split(', ')
+        tanggal = result[0]
+        waktu = result [1]
+
+        result = soup.find('div', {'class': "col-md-6 col-xs-6 gempabumi-detail no-padding"})
+        result = result.findChildren('li')
+        i = 0
+        magnitudo = None
+        kedalaman = None
+        ls = None
+        bt = None
+        lokasi = None
+        dirasakan = None
+
+
+        for res in result:
+            if i == 1:
+                magnitudo = res.text
+            elif i == 2:
+                kedalaman = res.text
+            elif i == 3:
+                koordinat = res.text.split(' - ')
+                ls = koordinat[0]
+                bt = koordinat[1]
+            elif i == 4:
+                lokasi = res.text
+            elif i == 5:
+                dirasakan = res.text
+
+
+            i = i + 1
 
 
         hasil = dict()
         hasil['tanggal'] = tanggal #'19 Oktober 2022'
         hasil['waktu'] = waktu #'13:27:28 WIB'
-        hasil['magnitudo'] = 3.4
-        hasil['kedalaman'] = '5 km'
-        hasil['lokasi'] = {'ls': 2.06, 'bt': 98.98}
-        hasil['pusat'] = 'Pusat gempa berada di darat 5 km TimurLaut Tapanuli Utara'
-        hasil['dirasakan']= 'Dirasakan (Skala MMI): III Sipoholon, III Tarutung'
+        hasil['magnitudo'] = magnitudo
+        hasil['kedalaman'] = kedalaman
+        hasil['koordinat'] = {'ls': ls, 'bt': bt}
+        hasil['lokasi'] = lokasi
+        hasil['dirasakan']= dirasakan
         return hasil
     else:
         return None
@@ -43,6 +72,6 @@ def tampilkan_data(result):
     print(f"Waktu {result['waktu']}")
     print(f"magnitudo {result['magnitudo']}")
     print(f"kedalaman {result['kedalaman']}")
-    print(f"lokasi: LS={result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
-    print(f"pusat {result['pusat']}")
+    print(f"koordinat: LS={result['koordinat']['ls']}, BT={result['koordinat']['bt']}")
+    print(f"lokasi {result['lokasi']}")
     print(f"dirasakan {result['dirasakan']}")
