7c46ecc325edf9e64f4dc445b173fffadb965596
viko-aldi <piccoaldi@gmail.com>
2022-10-20 19:01:50 +0700

    Second Commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 """
 Aplikasi Deteksi Gempa
 MODULARISASI DENGAN FUNCTION
+MODULARISASI DENGAN PACKAGE
 """
-from gempaterkini import ekstrasi_data, tampilkan_data
+import gempaterkini
 
 if __name__ == '__main__' :
     print('Aplikasi Utama')
-    result = ekstrasi_data()
-    tampilkan_data(result)
+    result = gempaterkini.ekstrasi_data()
+    gempaterkini.tampilkan_data(result)
