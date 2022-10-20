87674029256efd8aaaa30c37eb777bebd739a609
viko-aldi <piccoaldi@gmail.com>
2022-10-20 20:42:03 +0700

    Second Commit

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -1,3 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 def ekstrasi_data():
     """
     Tanggal : 19 Oktober 2022,
@@ -8,18 +12,32 @@ def ekstrasi_data():
     Pusat Gempa : Pusat gempa berada di darat 5 km TimurLaut Tapanuli Utara
     Dirasakan : Dirasakan (Skala MMI): III Sipoholon, III Tarutung
     """
-    hasil = dict()
-    hasil['tanggal'] = '19 Oktober 2022'
-    hasil['waktu'] = '13:27:28 WIB'
-    hasil['magnitudo'] = 3.4
-    hasil['kedalaman'] = '5 km'
-    hasil['lokasi'] = {'ls': 2.06, 'bt': 98.98}
-    hasil['pusat'] = 'Pusat gempa berada di darat 5 km TimurLaut Tapanuli Utara'
-    hasil['dirasakan']= 'Dirasakan (Skala MMI): III Sipoholon, III Tarutung'
+    try:
+        content = requests.get('https://bmkg.go.id')
+    except Exception :
+        return None
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html.parser')
+        tanggal = soup.find('span', {'class':'waktu'})
+        waktu = tanggal.text.split(', ')[1]
+        tanggal = tanggal.text.split(', ')[0]
 
 
-    return hasil
+        hasil = dict()
+        hasil['tanggal'] = tanggal #'19 Oktober 2022'
+        hasil['waktu'] = waktu #'13:27:28 WIB'
+        hasil['magnitudo'] = 3.4
+        hasil['kedalaman'] = '5 km'
+        hasil['lokasi'] = {'ls': 2.06, 'bt': 98.98}
+        hasil['pusat'] = 'Pusat gempa berada di darat 5 km TimurLaut Tapanuli Utara'
+        hasil['dirasakan']= 'Dirasakan (Skala MMI): III Sipoholon, III Tarutung'
+        return hasil
+    else:
+        return None
 def tampilkan_data(result):
+    if result is None:
+        print('Tidak Bisa menampilkan Data BMKG')
+        return
     print('Gempa Terahkir berdasarkan BMKG')
     print(f"Tanggal {result['tanggal']}")
     print(f"Waktu {result['waktu']}")
